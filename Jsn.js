b158335f3c20fbbdb8106f61f18b50ae3f91eee4
Manggla52 <Michaelmartinez427@gmail.com>
2024-06-20 11:58:43 -0500

    update

---
+++ Jsn.js
@@ -0,0 +1,49 @@
+//JavaScript lab--Loops
+
+// 1. have this function console.log() each time from the 
+//    drinks array. use the for loop inside the function.
+
+let drinks1 = ["early grey", "chamomile", "red zinger"];
+let drinks2 = ["mocha", "latte", "espresso"];
+
+function showDrinksOne(arr){
+
+}
+showDrinksOne(drinks1);
+showDrinksOne(drinks2);
+
+// 2. Have this function comsole.log() each item from te drinks aRRy.
+//    Use a WHILE LOOP inside the function
+
+function showDrinksTwo(arr){
+
+}
+showDrinksTwo(drinks1);
+
+// 3. please have tis function console log() the "loctaion" value of each obbject in the array. 
+//    Use the Loop to iterate over the array.
+
+let companies = [
+    { "name": "Mi crosoft", "location": "Redmond", "founded": 1975},
+    { "name": "Starbucks",  "location": "Seattle"},
+    { "name": "Netflix", "location": "Los Gatos"}
+]
+
+function getCompanyLocations(arr){
+
+}
+getCompanyLocations(companies);
+
+// 4. in the variable  soccerTeams, each inner array has player from a team. for thid function,
+//     use a FOR LOOP  to add the player "Messi" to each team. Then, conso le.log()
+//     the updated soccerTems array.
+
+let soccerTeams = [
+    ["J alverez", "E Haaland", "B Silva"],
+    [ "A Becker", "M Salah", "T Alxander=Arnold"],
+    [ "B Saka", "G Jesus", "L Troussard"]
+]
+function addMessiEachTeam(arr){
+
+}
+addMessiEachTeam(soccerTeams);
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # S-MBullyCamp
 # week 6-Lab CodexAcademy
-# [ ] Look over previous version and considers points to up date with JS, CSS, and HTML.
+# [x] Look over previous version and considers points to up date with JS, CSS, and HTML.
+# [X] Lost files some kind of way?@$%! Have to start from scratch....
 # [ ] create an Canva/Psuedo Code for update to S&M Bully Camp.
 # [ ] ii..
